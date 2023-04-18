066ec8cc93175c14726902b16fa11a1b22eed32d
franciscorodriguezsv24 <103607889+franciscorodriguezsv24@users.noreply.github.com>
2023-04-18 14:33:35 -0600

    TaskMaster V.1.0

---
+++ Task-creater/main.js
@@ -0,0 +1,64 @@
+const data = document.querySelector("#taskform")
+const $ = (elem) => document.querySelector(elem);
+
+
+
+data.addEventListener('submit', saveTask);
+
+function saveTask(e) {
+   
+   let title = $('#title').value;
+
+   let description = $('#description').value;
+
+   const task = {
+    title,
+    description
+   }
+
+if(localStorage.getItem('tasks') === null){
+    let tasks = [];
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+} else {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+    getTask();
+    $('#taskform').reset();
+    e.preventDefault();
+
+}
+
+function getTask(){
+ let tasks = JSON.parse(localStorage.getItem('tasks'));
+ let taskView = $('#tasks');
+
+ taskView.innerHTML = ``;
+
+ for(let i = 0; i < tasks.length; i++){
+   let title = tasks[i].title;
+   let description = tasks[i].description;
+
+   taskView.innerHTML += `<div class="card">
+   <div class="card-body">
+   <p>${title} - ${description}</p>
+   <a class="btn btn-danger text-white" onclick="deleteTask('${title}')">Delete</a>
+   </div>
+   </div>`
+ }
+}
+
+function deleteTask(title){
+let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+for(let i = 0; i < tasks.length; i++){
+if (tasks[i].title == title){
+    tasks.splice(i, 1);
+}
+}
+localStorage.setItem('tasks', JSON.stringify(tasks));
+getTask();
+}
+getTask()
